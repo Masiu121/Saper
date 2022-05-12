a8a4681ef19ee9d1db1f54912e868569a3fba6c9
Masiu121 <maks.kapolka@gmail.com>
2022-05-12 23:24:57 +0200

    Updated TileType.java

--- core/src/com/oxology/saper/util/TileType.java
+++ core/src/com/oxology/saper/util/TileType.java
@@ -5,17 +5,17 @@ import com.badlogic.gdx.graphics.Texture;
 public enum TileType {
     HIDDEN(new Texture("Tiles/tile_hidden.png")),
     EMPTY(new Texture("Tiles/tile_empty.png")),
-    BOMB(new Texture("Tiles/tile_bomb.png"));
-//    FLAG(new Texture("")),
-//    Q_MARK(new Texture("")),
-//    NUM_1(new Texture("")),
-//    NUM_2(new Texture("")),
-//    NUM_3(new Texture("")),
-//    NUM_4(new Texture("")),
-//    NUM_5(new Texture("")),
-//    NUM_6(new Texture("")),
-//    NUM_7(new Texture("")),
-//    NUM_8(new Texture(""));
+    BOMB(new Texture("Tiles/tile_bomb.png")),
+    FLAG(new Texture("Tiles/tile_flag.png")),
+    Q_MARK(new Texture("Tiles/tile_q_mark.png")),
+    NUM_1(new Texture("Tiles/tile_num_1.png")),
+    NUM_2(new Texture("Tiles/tile_num_2.png")),
+    NUM_3(new Texture("Tiles/tile_num_3.png")),
+    NUM_4(new Texture("Tiles/tile_num_4.png")),
+    NUM_5(new Texture("Tiles/tile_num_5.png")),
+    NUM_6(new Texture("Tiles/tile_num_6.png")),
+    NUM_7(new Texture("Tiles/tile_num_7.png")),
+    NUM_8(new Texture("Tiles/tile_num_8.png"));
 
     private Texture texture;
 
