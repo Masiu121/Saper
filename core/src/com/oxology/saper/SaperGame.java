7d1823cc58add75fae9ef27c161d04f0be906ff3
Masiu121 <maks.kapolka@gmail.com>
2022-05-12 22:33:10 +0200

    Updated SaperGame.java

--- core/src/com/oxology/saper/SaperGame.java
+++ core/src/com/oxology/saper/SaperGame.java
@@ -1,18 +1,37 @@
 package com.oxology.saper;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.oxology.saper.screens.MainGameScreen;
+import com.oxology.saper.util.Difficulty;
 
 public class SaperGame extends Game {
 	private SpriteBatch batch;
-	
+	public int gameViewportWidth;
+	public int gameViewportHeight;
+	public float gameViewportXProp;
+	public float gameViewportYProp;
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+		gameViewportWidth = 800;
+		gameViewportHeight = 450;
+		setup();
+		this.setScreen(new MainGameScreen(this, Difficulty.EASY));
+	}
+
+	public void setup() {
+		gameViewportXProp = (float) Gdx.graphics.getBackBufferWidth() / gameViewportWidth;
+		gameViewportYProp = (float) Gdx.graphics.getBackBufferHeight() / gameViewportHeight;
 	}
 
 	@Override
 	public void render () {
+		if(Gdx.input.isKeyJustPressed(Input.Keys.C))
+			Gdx.app.exit();
+
 		super.render();
 	}
 	
@@ -23,5 +42,13 @@ public class SaperGame extends Game {
 
 	public SpriteBatch getBatch() {
 		return batch;
+	}
+
+	public float getGameX() {
+		return Gdx.input.getX()/ gameViewportXProp;
+	}
+
+	public float getGameY() {
+		return gameViewportHeight-Gdx.input.getY()/ gameViewportYProp;
 	}
 }
