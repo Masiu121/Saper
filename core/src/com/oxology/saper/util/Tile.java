6779d087fdb7bf892b24d5abcffd923e1bf84f0b
Masiu121 <maks.kapolka@gmail.com>
2022-05-12 23:24:41 +0200

    Updated Tile.java

--- core/src/com/oxology/saper/util/Tile.java
+++ core/src/com/oxology/saper/util/Tile.java
@@ -7,6 +7,7 @@ public class Tile {
     private TileType type;
     private float scale;
     private boolean bomb;
+    public int bombsAround;
 
     public Tile(int x, int y, boolean bomb, float scale) {
         this.x = x;
@@ -20,11 +21,73 @@ public class Tile {
         batch.draw(type.getTexture(), this.x*16, this.y*16);
     }
 
-    public void reveal() {
-        if(bomb)
+    public void checkBombsAround(Tile[][] map) {
+        for(int i = -1; i < 2; i++) {
+            for(int j = -1; j < 2; j++) {
+                if(x+i >= 0 && x+i < map.length && y+j >= 0 && y+j < map.length) {
+                    if (map[x + i][y + j].bomb) {
+                        bombsAround++;
+                    }
+                }
+            }
+        }
+    }
+
+    public void reveal(Tile[][] map) {
+        if(bomb) {
             this.type = TileType.BOMB;
-        else
-            this.type = TileType.EMPTY;
+        } else {
+            if (bombsAround == 0) {
+                this.type = TileType.EMPTY;
+            } else {
+                switch (bombsAround) {
+                    case 1:
+                        this.type = TileType.NUM_1;
+                        break;
+                    case 2:
+                        this.type = TileType.NUM_2;
+                        break;
+                    case 3:
+                        this.type = TileType.NUM_3;
+                        break;
+                    case 4:
+                        this.type = TileType.NUM_4;
+                        break;
+                    case 5:
+                        this.type = TileType.NUM_5;
+                        break;
+                    case 6:
+                        this.type = TileType.NUM_6;
+                        break;
+                    case 7:
+                        this.type = TileType.NUM_7;
+                        break;
+                    case 8:
+                        this.type = TileType.NUM_8;
+                        break;
+                }
+            }
+        }
+
+
+//        for(int i = -1; i < 2; i++) {
+//            for(int j = -1; j < 2; j++) {
+//                if(x+i > 0 && x+i < map.length && y+j > 0 && y+j < map.length) {
+//                    if (map[x + i][y + j].type == TileType.HIDDEN && map[x + i][y + j].bombsAround == 0) {
+//                        map[x + i][y + j].reveal(map);
+//                    }
+//                }
+//            }
+//        }
+
+        if(bombsAround == 0)
+        for(int i = -1; i < 2; i++) {
+            for(int j = -1; j < 2; j++) {
+                if(x+i >= 0 && x+i < map.length && y+j >= 0 && y+j < map.length && map[x + i][y + j].type == TileType.HIDDEN) {
+                    map[x + i][y + j].reveal(map);
+                }
+            }
+        }
     }
 
     public int getX() {
