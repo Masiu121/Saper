a41003c2b9be7d001278909d6118fd4e98f5545d
Masiu121 <maks.kapolka@gmail.com>
2022-05-12 15:39:14 +0200

    Prepared main class for screens

--- core/src/com/oxology/saper/SaperGame.java
+++ core/src/com/oxology/saper/SaperGame.java
@@ -1,31 +1,27 @@
 package com.oxology.saper;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
 
-public class SaperGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+public class SaperGame extends Game {
+	private SpriteBatch batch;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		super.render();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+	}
+
+	public SpriteBatch getBatch() {
+		return batch;
 	}
 }
