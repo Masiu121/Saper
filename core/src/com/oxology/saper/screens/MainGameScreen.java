83126ae9bb633d4f3aeee8b489b0d7b096a7871d
Masiu121 <maks.kapolka@gmail.com>
2022-05-12 15:51:33 +0200

    Added MainGameScreen.java

---
+++ core/src/com/oxology/saper/screens/MainGameScreen.java
@@ -0,0 +1,50 @@
+package com.oxology.saper.screens;
+
+import com.badlogic.gdx.Screen;
+import com.oxology.saper.SaperGame;
+import com.oxology.saper.util.Difficulty;
+
+public class MainGameScreen implements Screen {
+    private SaperGame game;
+    private Difficulty difficulty;
+
+    public MainGameScreen(SaperGame game, Difficulty difficulty) {
+        this.game = game;
+        this.difficulty = difficulty;
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
