959d0e89f2fd104f504461ea351d31a654e64544
Masiu121 <maks.kapolka@gmail.com>
2022-05-12 22:35:38 +0200

    Updated Tile.java

--- core/src/com/oxology/saper/util/Tile.java
+++ core/src/com/oxology/saper/util/Tile.java
@@ -1,4 +1,37 @@
 package com.oxology.saper.util;
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 public class Tile {
+    private int x, y;
+    private TileType type;
+    private float scale;
+    private boolean bomb;
+
+    public Tile(int x, int y, boolean bomb, float scale) {
+        this.x = x;
+        this.y = y;
+        this.bomb = bomb;
+        this.scale = scale;
+        this.type = TileType.HIDDEN;
+    }
+
+    public void draw(SpriteBatch batch) {
+        batch.draw(type.getTexture(), this.x*16, this.y*16);
+    }
+
+    public void reveal() {
+        if(bomb)
+            this.type = TileType.BOMB;
+        else
+            this.type = TileType.EMPTY;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 }
